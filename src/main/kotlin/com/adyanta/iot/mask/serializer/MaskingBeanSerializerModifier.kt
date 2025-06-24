5386607b6562bf069a323b6c8f19ee931c93a52f
kuldeepbisen <kdbisen@gmail.com>
2025-06-24 09:48:22 +0530

    init

--- src/main/kotlin/com/adyanta/iot/mask/serializer/MaskingBeanSerializerModifier.kt
+++ src/main/kotlin/com/adyanta/iot/mask/serializer/MaskingBeanSerializerModifier.kt
@@ -1,21 +1,43 @@
 package com.adyanta.iot.mask.serializer
 
 import com.fasterxml.jackson.databind.BeanDescription
+import com.fasterxml.jackson.databind.SerializationConfig
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter
+import com.fasterxml.jackson.databind.ser.BeanSerializerBuilder
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier
 
-class MaskingBeanSerializerModifier(private val fieldsToMask: Set<String>) : BeanSerializerModifier() {
-    override fun changeProperties(
-        config: com.fasterxml.jackson.databind.SerializationConfig,
+
+class MaskingBeanSerializerModifier(
+    private val fieldsToMask: Set<String>,
+    private val deep: Boolean = false
+) : BeanSerializerModifier() {
+
+    override fun updateBuilder(
+        config: SerializationConfig,
         beanDesc: BeanDescription,
-        beanProperties: MutableList<BeanPropertyWriter>
-    ): MutableList<BeanPropertyWriter> {
-        for (writer in beanProperties) {
-            val fullPath = "${beanDesc.beanClass.simpleName.lowercase()}.${writer.name}"
-            if (fieldsToMask.contains(writer.name) || fieldsToMask.contains(fullPath)) {
+        builder: BeanSerializerBuilder
+    ): BeanSerializerBuilder {
+        val prefix = beanDesc.beanClass.simpleName.replaceFirstChar { it.lowercase() }
+        val originalWriters = builder.properties
+
+        val newWriters = originalWriters.map { writer ->
+            val fullPath = buildFullPath(prefix, writer.name)
+            if (shouldMask(writer.name, fullPath)) {
+                println("[MASKING] Matched field: '${writer.name}' at path: '$fullPath'")
                 writer.assignSerializer(MaskingSerializer())
             }
-        }
-        return beanProperties
+            writer
+        }.toMutableList()
+
+        builder.setProperties(newWriters)
+        return builder
+    }
+
+    private fun buildFullPath(prefix: String, fieldName: String): String {
+        return if (deep) "$prefix.$fieldName" else fieldName
+    }
+
+    private fun shouldMask(name: String, fullPath: String): Boolean {
+        return fieldsToMask.contains(name) || fieldsToMask.contains(fullPath)
     }
 }
